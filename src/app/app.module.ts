f07980b79f11ecee749686a5fea3383a24cfc7c6
99pmore <morenomartin.pablo@gmail.com>
2023-02-08 17:19:57 +0100

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     HomeComponent,
     GameComponent,
-    FlagsComponent
+    FlagsComponent,
   ],
   imports: [
     BrowserModule,
